ab507e8e6b16c10a15f99e8825f54e7f91e4c2d9
9sixt1nwza <merrysubs4t@gmail.com>
2025-04-22 12:57:53 +0700

    Update Attack.lua

--- Fetching/Utils/Attack.lua
+++ Fetching/Utils/Attack.lua
@@ -1,20 +1,83 @@
+local rs = game:GetService("ReplicatedStorage")
+local players = game:GetService("Players")
+local client = players.LocalPlayer
+local modules = rs:WaitForChild("Modules")
+local net = modules:WaitForChild("Net")
+local charFolder = workspace:WaitForChild("Characters")
+local enemyFolder = workspace:WaitForChild("Enemies")
+
+local Module = {
+    AttackCooldown = tick()
+}
+local CachedChars = {}
+
+function Module.IsAlive(Char)
+    if not Char then return nil end
+    local Hum = CachedChars[Char] or Char:FindFirstChildOfClass("Humanoid")
+    if Hum then
+        CachedChars[Char] = Hum
+        return Hum.Health > 0
+    end
+    return false
+end
+
+local Settings = {
+    ClickDelay = 0.01,
+    AutoClick = true
+}
+
+local AttackModule = {
+    NextAttack = 0,
+    Distance = 70,
+    attackMobs = true,
+    attackPlayers = true
+}
+
+local RegisterAttack = net:WaitForChild("RE/RegisterAttack")
+local RegisterHit = net:WaitForChild("RE/RegisterHit")
+
+function AttackModule:AttackEnemy(EnemyHead, Table)
+    if EnemyHead and client:DistanceFromCharacter(EnemyHead.Position) < self.Distance then
+        if not self.FirstAttack then
+            RegisterAttack:FireServer(Settings.ClickDelay or 0)
+            self.FirstAttack = true
+        end
+        RegisterHit:FireServer(EnemyHead, Table or {})
+    end
+end
+
+function AttackModule:AttackNearest()
+    local args = {nil, {}}
+    for _, Enemy in ipairs(enemyFolder:GetChildren()) do
+        local humanoidPart = Enemy:FindFirstChild("HumanoidRootPart")
+        if humanoidPart and client:DistanceFromCharacter(humanoidPart.Position) < self.Distance then
+            local upperTorso = Enemy:FindFirstChild("UpperTorso")
+            if not args[1] then
+                args[1] = upperTorso
+            else
+                table.insert(args[2], {Enemy, upperTorso})
+            end
+        end
+    end
+
+    self:AttackEnemy(unpack(args))
+
+    for _, Char in ipairs(charFolder:GetChildren()) do
+        if Char ~= client.Character then
+            self:AttackEnemy(Char:FindFirstChild("UpperTorso"))
+        end
+    end
+
+    if not self.FirstAttack then
+        task.wait(0.01)
+    end
+end
+
+function AttackModule:BladeHits()
+    self:AttackNearest()
+    self.FirstAttack = false
+end
+
 return function()
-	for _, v in next, workspace.Enemies:GetChildren() do
-		if v.Humanoid.Health > 0 and v:FindFirstChild("HumanoidRootPart") and (v.HumanoidRootPart.Position - game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= tonumber(60) then
-			game:GetService("ReplicatedStorage").Modules.Net["RE/RegisterAttack"]:FireServer(0)
-			local args = {
-				[1] = v:FindFirstChild("HumanoidRootPart"),
-				[2] = {}
-			}
-			for _, e in next, workspace:WaitForChild("Enemies"):GetChildren() do
-				if e:FindFirstChild("Humanoid") and e.Humanoid.Health > 0 then
-					table.insert(args[2], {
-						[1] = e,
-						[2] = e:FindFirstChild("HumanoidRootPart") or e:FindFirstChildOfClass("BasePart")
-					})
-				end
-			end
-			game:GetService("ReplicatedStorage").Modules.Net["RE/RegisterHit"]:FireServer(unpack(args))
-		end
-	end
+    AttackModule:BladeHits()
 end
