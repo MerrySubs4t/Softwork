5f9635a0630bd065add75b4d01d3a2dc482afb48
9sixt1nwza <merrysubs4t@gmail.com>
2025-04-25 09:03:29 +0700

    Update Attack.lua

--- Fetching/Utils/Attack.lua
+++ Fetching/Utils/Attack.lua
@@ -1,66 +1,103 @@
-local v21 = game.Players.LocalPlayer
-function FindEnemiesInRange(v223, v224)
-	local v225 = (v21.Character or v21.CharacterAdded:Wait()):GetPivot().Position
-	local v226 = nil
-	for v471, v472 in ipairs(v224) do
-		if (not v472:GetAttribute("IsBoat") and v472:FindFirstChildOfClass("Humanoid") and (v472.Humanoid.Health > 0)) then
-			local v671 = v472:FindFirstChild("Head")
-			if (v671 and ((v225 - v671.Position).Magnitude <= 60)) then
-				if (v472 ~= v21.Character) then
-					table.insert(v223, {
-						v472,
-						v671
-					})
-					v226 = v671
+
+
+local env = (getgenv or getrenv or getfenv)()
+local rs = game:GetService("ReplicatedStorage")
+local players = game:GetService("Players")
+local client = players.LocalPlayer
+local modules = rs:WaitForChild("Modules")
+local net = modules:WaitForChild("Net")
+local charFolder = workspace:WaitForChild("Characters")
+local enemyFolder = workspace:WaitForChild("Enemies")
+
+local Module = {
+	AttackCooldown = tick()
+}
+local CachedChars = {}
+
+function Module.IsAlive(Char: Model?): boolean
+	if not Char then return nil end
+	local Hum = CachedChars[Char] or Char:FindFirstChildOfClass("Humanoid")
+	if Hum then
+		CachedChars[Char] = Hum
+		return Hum.Health > 0
+	end
+	return false
+end
+
+local HIT_FUNCTION; task.defer(function()
+	local PlayerScripts = client:WaitForChild("PlayerScripts")
+	local LocalScript = PlayerScripts:FindFirstChildOfClass("LocalScript")
+
+	while not LocalScript do
+		client.PlayerScripts.ChildAdded:Wait()
+		LocalScript = PlayerScripts:FindFirstChildOfClass("LocalScript")
+	end
+
+	if getsenv then
+		local Success, ScriptEnv = pcall(getsenv, LocalScript)
+
+		if Success and ScriptEnv then
+			HIT_FUNCTION = ScriptEnv._G.SendHitsToServer
+		end
+	end
+end)
+
+Module.FastAttack = (function()
+	if env._trash_attack then 
+		return function() env._trash_attack:BladeHits() end
+	end
+
+	local AttackModule = {
+		NextAttack = 0,
+		Distance = 70,
+		attackMobs = true,
+		attackPlayers = true
+	}
+
+	local RegisterAttack = net:WaitForChild("RE/RegisterAttack")
+	local RegisterHit = net:WaitForChild("RE/RegisterHit")
+
+	function AttackModule:AttackEnemy(EnemyHead, Table)
+		if EnemyHead and client:DistanceFromCharacter(EnemyHead.Position) < self.Distance then
+			if HIT_FUNCTION then 
+				HIT_FUNCTION(EnemyHead, Table or {})
+			else
+				RegisterHit:FireServer(EnemyHead, Table or {})
+			end
+			RegisterAttack:FireServer(0)
+		end
+	end
+
+	function AttackModule:AttackNearest()
+		local args = {nil, {}}
+		for _, Enemy in ipairs(enemyFolder:GetChildren()) do
+			local humanoidPart = Enemy:FindFirstChild("HumanoidRootPart")
+			if humanoidPart and client:DistanceFromCharacter(humanoidPart.Position) < self.Distance then
+				local upperHead = Enemy:FindFirstChild("HumanoidRootPart")
+				if not args[1] then
+					args[1] = upperHead
+				else
+					table.insert(args[2], {Enemy, upperHead})
 				end
 			end
 		end
-	end
-	for v473, v474 in ipairs(game.Players:GetPlayers()) do
-		if (v474.Character and (v474 ~= v21)) then
-			local v672 = v474.Character:FindFirstChild("Head")
-			if (v672 and ((v225 - v672.Position).Magnitude <= 60)) then
-				table.insert(v223, {
-					v474.Character,
-					v672
-				})
-				v226 = v672
+
+		self:AttackEnemy(unpack(args))
+
+		for _, Char in ipairs(charFolder:GetChildren()) do
+			if Char ~= client.Character then
+				self:AttackEnemy(Char:FindFirstChild("HumanoidRootPart"))
 			end
 		end
 	end
-	return v226
-end
-function GetEquippedTool()
-	local v227 = v21.Character
-	if not v227 then
-		return nil
+
+	function AttackModule:BladeHits()
+		self:AttackNearest()
 	end
-	for v475, v476 in ipairs(v227:GetChildren()) do
-		if v476:IsA("Tool") then
-			return v476
-		end
+
+	env._trash_attack = AttackModule
+
+	return function()
+		AttackModule:BladeHits()
 	end
-	return nil
-end
-return function()
-	local v228 = {}
-	local v229 = game:GetService("Workspace").Enemies:GetChildren()
-	local v230 = FindEnemiesInRange(v228, v229)
-	if not v230 then
-		return
-	end
-	local v231 = GetEquippedTool()
-	if not v231 then
-		return
-	end
-	pcall(function()
-		if (# v228 > 0) then
-			game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Net"):WaitForChild("RE/RegisterAttack"):FireServer(1e-999)
-			game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Net"):WaitForChild("RE/RegisterHit"):FireServer(v230, v228)
-			game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Net"):WaitForChild("RE/RegisterHit"):FireServer(v230, v228)
-			game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("Net"):WaitForChild("RE/RegisterHit"):FireServer(v230, v228)
-		else
-			task.wait(1e-999)
-		end
-	end)
-end
+end)()
